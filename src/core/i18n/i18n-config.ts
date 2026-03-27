9bec1091cdd179c4f1ec07f21c2fb0dc55fae714
Fernando <fernandoalugo@gmail.com>
2026-03-27 16:26:25 +0000

    refactor: change order

--- src/core/i18n/i18n-config.ts
+++ src/core/i18n/i18n-config.ts
@@ -2,10 +2,10 @@ import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import { DefaultNamespace, Language } from "./i18n-model";
-import HttpApi from "i18next-http-backend";
+import Backend from "i18next-http-backend";
 
+i18n.use(Backend);
 i18n.use(initReactI18next);
-i18n.use(HttpApi);
 i18n.use(LanguageDetector);
 
 void i18n.init({
